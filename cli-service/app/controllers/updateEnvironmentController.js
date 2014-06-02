4990033f2596c4d81a8305a9a9c48acc89135250
Alan Stice <alan@associativeintelligence.org>
2014-06-02 16:27:01 -0500

    Start implementing the UpdateEnvironmentController - Added parameter definition - Locked location for orgs in web interface based on SfOpticon not being able to update it

--- cli-service/app/controllers/updateEnvironmentController.js
+++ cli-service/app/controllers/updateEnvironmentController.js
@@ -4,6 +4,33 @@
  * Created by Alan on 5/27/2014.
  */
 function UpdateEnvironmentController() {
+    this.paramDefinition = {
+        org: {
+            required: true,
+            type: 'string',
+            description: 'The organization to update'
+        },
+        name: {
+            required: false,
+            type: 'string',
+            description: 'The friendly name for the organization.'
+        },
+        username: {
+            required: false,
+            type: 'string',
+            description: 'The username for accessing the org.'
+        },
+        password: {
+            required: false,
+            type: 'string',
+            description: 'The password for accessing the org.'
+        },
+        production: {
+            required: false,
+            type: 'boolean',
+            description: 'Flag for if the org is the production org.'
+        }
+    }
 }
 
 module.exports = UpdateEnvironmentController;
